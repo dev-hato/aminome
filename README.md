91f62f298613405040f7d87ba19e053b8121bfca
nakkaa <10798675+nakkaa@users.noreply.github.com>
2023-05-12 12:52:56 +0900

    first version

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # aminome
-Add all misskey notes to Meilisearch
+
+**A**dd all **mi**sskey **no**tes to **Me**ilisearch.
+
+## Target version
+
+- Misskey v13.12.1
+- Meilisearch v1.1.1
+
+## How to use
+
+Install python package.
+
+```sh
+pip3 install -r requirements.txt
+```
+
+Configure and run.
+
+```sh
+python3 ./aminome.py
+```
+
+## References
+
+- [dump_misskey_note_data.py](https://gist.github.com/CyberRex0/d481c4c2be6dc47fee4b50cefadf2074)
+- [mattyatea/misskey-meilisearch-oldnote-index](https://github.com/mattyatea/misskey-meilisearch-oldnote-index)
---
+++ aminome.py
@@ -0,0 +1,58 @@
+# Register local notes on Misskey to Meilisearch
+import psycopg2
+import psycopg2.extras
+import orjson
+import requests
+
+# postgresql config
+db = psycopg2.connect(
+    host='localhost',
+    user='misskey-user',
+    password='password',
+    database='misskey',
+    port=5432,
+    cursor_factory=psycopg2.extras.DictCursor
+)
+
+# meilisearch config
+api_key = "APIKEY"
+url = "http://localhost:7700/indexes/notes/documents?primaryKey=id"
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
+}
+
+lmt = 100000
+ofs = 0
+
+notes = []
+
+while True:
+    with db.cursor() as cur:
+        cur.execute('SELECT "id", "createdAt", "userId", "userHost", "channelId", "cw", "text" FROM "note" \
+                    WHERE ("note"."visibility" = \'public\' OR "note"."visibility" = \'home\') AND\
+                    ("note"."text" IS NOT NULL) AND\
+                    ( "note"."uri" IS NULL) \
+                    LIMIT '  + str(lmt) + ' OFFSET ' + str(ofs))
+        qnotes = cur.fetchall()
+        if not qnotes:
+            break
+    for note in qnotes:
+        notes.append({
+            'id': note['id'],
+            'text': note['text'],
+            'createdAt': int(note['createdAt'].timestamp() * 1000),
+            'userId': note['userId'],
+            'userHost': note['userHost'],
+            'channelId': note['channelId'],
+            'cw': note['cw'],
+            'text': note['text']
+        })
+    print(f'{ofs=} {lmt=} {len(notes)=}')
+    ofs = ofs + lmt
+
+db.close()
+
+response = requests.post(url, data=orjson.dumps(notes), headers=headers)
+
+print(response.content)
